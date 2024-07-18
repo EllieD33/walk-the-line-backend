3d0ecdca4d1fb0df22a2b7fed97d6544b908cfae
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-07-18 17:37:12 +0100

    refactors walks model for readability and  consistent error handling

--- models/walks.model.js
+++ models/walks.model.js
@@ -1,7 +1,6 @@
 const db = require("../db/connection")
 const format = require('pg-format');
 
-
 const fetchWalks = async (walk_id, creator_id, difficultyRequired, minDistance, maxDistance) => {
     let queryText = `
         SELECT wlk.*, usr.username
@@ -26,53 +25,52 @@ const fetchWalks = async (walk_id, creator_id, difficultyRequired, minDistance, 
     return rows;
 };
 
-
 const createTrail = async (trailObj) => {
     const { walk, locations } = trailObj;
 
-    const insertWalkText = `
+    const walkQueryText = `
         INSERT INTO walks (
             creator_id, title, description, distance_km,
             ascent, difficulty, start_latitude, start_longitude, start_altitude
         ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
         RETURNING *`;
 
-    const walkValues = [
+    const walkQueryValues = [
         walk.creator_id, walk.title, walk.description, walk.distance_km,
         walk.ascent, walk.difficulty, walk.start_latitude, walk.start_longitude, walk.start_altitude
     ];
 
-    const { rows: [insertedWalk] } = await db.query(insertWalkText, walkValues);
+    const { rows: [insertedWalk] } = await db.query(walkQueryText, walkQueryValues);
 
     if (!insertedWalk) {
-        throw new Error('Failed to insert walk');
+        return Promise.reject({ status: 500, msg: 'Failed to insert walk' });
     }
 
     const walkId = insertedWalk.walk_id;
 
-    const locationsValues = locations.map(loc => [
-        walkId, loc.latitude, loc.longitude, loc.altitude
+    const locationsQueryValues = locations.map(location => [
+        walkId, location.latitude, location.longitude, location.altitude
     ]);
 
-    const insertLocationsText = format(`
+    const locationsQueryText = format(`
         INSERT INTO walk_location_points (walk_id, latitude, longitude, altitude)
         VALUES %L
-    `, locationsValues);
+    `, locationsQueryValues);
 
-    await db.query(insertLocationsText);
+    await db.query(locationsQueryText);
 
     return { walk: insertedWalk };
 }
 
 const removeWalk = async (walk_id) => {
     if (!Number.isInteger(walk_id)) {
-        return Promise.reject({ status: 400, msg: "Bad Request" })
+        return Promise.reject({ status: 400, msg: "Bad Request" });
     }
 
-    let deleteStr = 'DELETE FROM walks WHERE walk_id = $1;'
-    const deleteResult = await db.query(deleteStr, [walk_id])
+    let queryText = 'DELETE FROM walks WHERE walk_id = $1;';
+    const deletionResult = await db.query(queryText, [walk_id]);
 
-    if (deleteResult.rowCount === 0) {
+    if (deletionResult.rowCount === 0) {
         return Promise.reject({ status: 404, msg: "Not Found" });
     }
 }

d3983becd354ea8edc7c150e7218f5008c5af6f4
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-07-18 18:00:31 +0100

    adds try catch to walks model

--- models/users.model.js
+++ models/users.model.js
@@ -2,30 +2,42 @@ const db = require("../db/connection");
 const bcrypt = require("bcrypt");
 
 const fetchUser = async (username, password) => {
-    const userQueryStr = `SELECT * 
+    try {
+      const userQueryStr = `SELECT * 
                         FROM users 
                         WHERE username = $1;`;
-    const user = await db.query(userQueryStr, [username]);
-    if (!user.rows.length) return null;
+      const user = await db.query(userQueryStr, [username]);
+      if (!user.rows.length) return null;
 
-    const storedPassword = user.rows[0].password;
-    const isPasswordValid = await bcrypt.compare(password, storedPassword);
+      const storedPassword = user.rows[0].password;
+      const isPasswordValid = await bcrypt.compare(password, storedPassword);
 
-    if (!isPasswordValid) return null;
+      if (!isPasswordValid) return null;
 
-    return user.rows[0];
+      return user.rows[0];
+  } catch (err) {
+    return Promise.reject({ status: 401, msg: "Invalid credentials" });
+  }
 };
 
 const fetchUserByUsername = async (username) => {
-  const queryStr = `SELECT * FROM users WHERE username = $1`;
-  const result = await db.query(queryStr, [username]);
-  return result.rows[0];
+  try {
+    const queryStr = `SELECT * FROM users WHERE username = $1`;
+    const result = await db.query(queryStr, [username]);
+    return result.rows[0];
+  } catch (err) {
+    return Promise.reject({ status: 404, msg: "Not found" });
+  }
 };
 
 const createUser =  async (username, hashedPassword) => {
-  const insertStr = `INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *`;
-  const result = await db.query(insertStr, [username, hashedPassword]);
-  return result.rows[0];
+  try {
+    const insertStr = `INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *`;
+    const result = await db.query(insertStr, [username, hashedPassword]);
+    return result.rows[0];
+  } catch (err) {
+    return Promise.reject({ status: 500, msg: "Failed to create user" });
+  }
 }
 
 module.exports = { fetchUser, fetchUserByUsername, createUser };
